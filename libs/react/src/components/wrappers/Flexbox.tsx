71f25a8ca3c4c97f9fd381eaaa302ec6b89823ed
jt <unluckycricketgames@gmail.com>
2022-11-11 13:30:05 -0500

    flexbox now forwards refs

--- libs/react/src/components/wrappers/Flexbox.tsx
+++ libs/react/src/components/wrappers/Flexbox.tsx
@@ -1,4 +1,4 @@
-import { HTMLProps } from 'react';
+import { forwardRef, HTMLProps } from 'react';
 import { formatClassName } from '../../util/util-functions';
 
 interface BaseFlexboxProps {
@@ -18,95 +18,101 @@ export interface FlexboxProps
 /**
  * A wrapper that allows for rapid and simple assembly of flexboxes for common use cases.
  */
-export const Flexbox = ({
-  className,
-  children,
-  style,
-  direction = 'row',
-  reverseDirection = false,
-  horizontalAlignment = 'left',
-  verticalAlignment = 'top',
-  wrap = true,
-  reverseWrap = false,
-  spacing = '0.5rem',
-  ...otherProps
-}: FlexboxProps) => {
-  const flexWrap = `${wrap ? 'wrap' : 'nowrap'}${
-    reverseWrap && wrap ? '-reverse' : ''
-  }`;
-  const flexFlow = `${direction}${
-    reverseDirection ? '-reverse' : ''
-  } ${flexWrap}`;
+export const Flexbox = forwardRef<HTMLDivElement, FlexboxProps>(
+  (
+    {
+      className,
+      children,
+      style,
+      direction = 'row',
+      reverseDirection = false,
+      horizontalAlignment = 'left',
+      verticalAlignment = 'top',
+      wrap = true,
+      reverseWrap = false,
+      spacing = '0.5rem',
+      ...otherProps
+    },
+    ref
+  ) => {
+    const flexWrap = `${wrap ? 'wrap' : 'nowrap'}${
+      reverseWrap && wrap ? '-reverse' : ''
+    }`;
+    const flexFlow = `${direction}${
+      reverseDirection ? '-reverse' : ''
+    } ${flexWrap}`;
 
-  const mainAxis = direction === 'row' ? 'x' : 'y';
-  const crossAxis = mainAxis === 'x' ? 'y' : 'x';
+    const mainAxis = direction === 'row' ? 'x' : 'y';
+    const crossAxis = mainAxis === 'x' ? 'y' : 'x';
 
-  let horizontalAligmentToFlexTerms = '';
-  switch (horizontalAlignment) {
-    case 'left':
-      horizontalAligmentToFlexTerms =
-        mainAxis === 'x' && reverseDirection ? 'flex-end' : 'flex-start';
-      break;
-    case 'center':
-      horizontalAligmentToFlexTerms = 'center';
-      break;
-    case 'right':
-      horizontalAligmentToFlexTerms =
-        mainAxis === 'x' && reverseDirection ? 'flex-start' : 'flex-end';
-      break;
+    let horizontalAligmentToFlexTerms = '';
+    switch (horizontalAlignment) {
+      case 'left':
+        horizontalAligmentToFlexTerms =
+          mainAxis === 'x' && reverseDirection ? 'flex-end' : 'flex-start';
+        break;
+      case 'center':
+        horizontalAligmentToFlexTerms = 'center';
+        break;
+      case 'right':
+        horizontalAligmentToFlexTerms =
+          mainAxis === 'x' && reverseDirection ? 'flex-start' : 'flex-end';
+        break;
+    }
+
+    let verticalAlignmentToFlexTerms = '';
+    switch (verticalAlignment) {
+      case 'top':
+        verticalAlignmentToFlexTerms =
+          mainAxis === 'y' && reverseDirection ? 'flex-end' : 'flex-start';
+        break;
+      case 'center':
+        verticalAlignmentToFlexTerms = 'center';
+        break;
+      case 'bottom':
+        verticalAlignmentToFlexTerms =
+          mainAxis === 'y' && reverseDirection ? 'flex-start' : 'flex-end';
+        break;
+    }
+
+    // justify-content drives the alignment and spacing along the main axis.
+    const justifyContent =
+      mainAxis === 'x'
+        ? horizontalAligmentToFlexTerms
+        : verticalAlignmentToFlexTerms;
+
+    // align-items drives the alignment along the cross axis
+    const alignItems =
+      crossAxis === 'x'
+        ? horizontalAligmentToFlexTerms
+        : verticalAlignmentToFlexTerms;
+
+    // align-content drives the flex container's lines when multiline, and drives spacing and alignment along the
+    // cross axis.
+    const alignContent =
+      crossAxis === 'x'
+        ? horizontalAligmentToFlexTerms
+        : verticalAlignmentToFlexTerms;
+
+    return (
+      <div
+        className={formatClassName('jtjs-flexbox', className)}
+        style={{
+          display: 'flex',
+          flexFlow,
+          justifyContent,
+          alignItems,
+          alignContent,
+          gap: spacing,
+          ...style,
+        }}
+        ref={ref}
+        {...otherProps}
+      >
+        {children}
+      </div>
+    );
   }
-
-  let verticalAlignmentToFlexTerms = '';
-  switch (verticalAlignment) {
-    case 'top':
-      verticalAlignmentToFlexTerms =
-        mainAxis === 'y' && reverseDirection ? 'flex-end' : 'flex-start';
-      break;
-    case 'center':
-      verticalAlignmentToFlexTerms = 'center';
-      break;
-    case 'bottom':
-      verticalAlignmentToFlexTerms =
-        mainAxis === 'y' && reverseDirection ? 'flex-start' : 'flex-end';
-      break;
-  }
-
-  // justify-content drives the alignment and spacing along the main axis.
-  const justifyContent =
-    mainAxis === 'x'
-      ? horizontalAligmentToFlexTerms
-      : verticalAlignmentToFlexTerms;
-
-  // align-items drives the alignment along the cross axis
-  const alignItems =
-    crossAxis === 'x'
-      ? horizontalAligmentToFlexTerms
-      : verticalAlignmentToFlexTerms;
-
-  // align-content drives the flex container's lines when multiline, and drives spacing and alignment along the
-  // cross axis.
-  const alignContent =
-    crossAxis === 'x'
-      ? horizontalAligmentToFlexTerms
-      : verticalAlignmentToFlexTerms;
-
-  return (
-    <div
-      className={formatClassName('jtjs-flexbox', className)}
-      style={{
-        display: 'flex',
-        flexFlow,
-        justifyContent,
-        alignItems,
-        alignContent,
-        gap: spacing,
-        ...style,
-      }}
-      {...otherProps}
-    >
-      {children}
-    </div>
-  );
-};
+);
 
 export default Flexbox;
